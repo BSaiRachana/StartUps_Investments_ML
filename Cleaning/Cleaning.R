0978f083f6031c95641caf861504575f5d407bab
PVineeth <vineeth.pen@outlook.com>
2020-04-24 14:18:17 -0500

    Converted first_funding_at and last_funding_at to 3 different columns
    Signed-off-by: PVineeth <vineeth.pen@outlook.com>

--- Cleaning/Cleaning.R
+++ Cleaning/Cleaning.R
@@ -1,3 +1,5 @@
+library(lubridate)
+
 setwd("/Users/vineethpenugonda/Documents/Academics/Masters/Semester IV/IST 5535/Projects/StartUps_Investments_ML/Dataset/")
 dat <- read.csv("/Users/vineethpenugonda/Documents/Academics/Masters/Semester IV/IST 5535/Projects/StartUps_Investments_ML/Dataset/US_StartUps_Investments_Data.csv", na.strings = c("", "NA"), stringsAsFactors = FALSE)
 str(dat)
@@ -12,21 +14,37 @@ dat <- dat[complete.cases(dat),]
 dat$market <- trimws(dat$market, which = c("both"))
 
 # Remove city, homepage_URL and X
-dat$city <- NULL
 dat$homepage_url <- NULL
 dat$X <- NULL
+dat$permalink <- NULL
+dat$country_code <- NULL
 
 # Strip values in columns
 # Remove Year
 dat$founded_month = substring(dat$founded_month,6)
+dat$founded_month = as.numeric(dat$founded_month)
 # Remove Year
 dat$founded_quarter = substring(dat$founded_quarter,6)
 # YY/MM/DD. We only need the day.
 dat$founded_at = substring(dat$founded_at,9)
+dat$founded_at = as.numeric(dat$founded_at)
 
 # Date DataType
 dat$first_funding_at <- as.Date(dat$first_funding_at)
 dat$last_funding_at <- as.Date(dat$last_funding_at)
+
+# Separate date into three columns
+dat$first_funding_day <- day(ymd(dat$first_funding_at))
+dat$first_funding_month <- month(ymd(dat$first_funding_at)) 
+dat$first_funding_year <- year(ymd(dat$first_funding_at))
+
+dat$last_funding_day <- day(ymd(dat$last_funding_at))
+dat$last_funding_month <- month(ymd(dat$last_funding_at)) 
+dat$last_funding_year <- year(ymd(dat$last_funding_at))
+
+# Remove Funding Dates
+dat$first_funding_at <- NULL
+dat$last_funding_at <- NULL
 
 str(dat)
 
