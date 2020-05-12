e9d6bda305f26fb468d7a37fb1b13f8cf833b8dc
PVineeth <vineeth.pen@outlook.com>
2020-05-12 02:23:33 -0500

    Add all other changes
    Signed-off-by: PVineeth <vineeth.pen@outlook.com>

--- Cleaning/Commands.R
+++ Cleaning/Commands.R
@@ -1,2 +1,8 @@
-source('~/Documents/Academics/Masters/Semester IV/IST 5535/Projects/StartUps_Investments_ML/Cleaning/Cleaning.R')
-source('~/Documents/Academics/Masters/Semester IV/IST 5535/Projects/StartUps_Investments_ML/Cleaning/Data_Encoding.R')
+#source('~/Documents/Academics/Masters/Semester IV/IST 5535/Projects/StartUps_Investments_ML/Cleaning/Cleaning.R')
+#source('~/Documents/Academics/Masters/Semester IV/IST 5535/Projects/StartUps_Investments_ML/Cleaning/Data_Encoding.R')
+
+str(dat)
+
+datWork <- dat
+datWork$market <- factor(datWork$market)
+str(datWork)
